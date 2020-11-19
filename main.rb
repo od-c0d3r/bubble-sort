de3508dbd75534e2a31f9e2d2ba2d21ccd239059
rashad <omar_rashad@aol.com>
2020-11-19 17:22:19 +0200

    Creating #bubble_sorting_by method

---
+++ main.rb
@@ -0,0 +1,46 @@
+  
+def bubble_sort(array)
+  flag = 1
+  while flag != 0
+    flag = 0
+    index = 0 
+    while index < array.size-1
+      a = array[index]
+      b = array[index+1]
+      if a > b
+        array[index] = b
+        array[index+1] = a
+        flag = 1
+      end
+      index += 1
+    end
+  end
+  array
+end
+
+# puts bubble_sort([4,3,5,2,100,1,800,500,4,300])
+
+def bubble_sort_by(array)
+  flag = 1
+  while flag != 0
+    flag = 0
+    index = 0 
+    while index < array.size-1
+      left = array[index]
+      right = array[index+1]
+      yield(left,right)
+      if left.length > right.length
+        array[index] = right
+        array[index+1] = left
+        flag = 1
+      end
+      print left.length - right.length
+      index += 1
+    end
+  end
+  array
+end
+
+puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}
+
+
